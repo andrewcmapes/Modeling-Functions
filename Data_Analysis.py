18aeb68c0ca7b6370573967a9748c954cb7cc0ff
Andrew Mapes <Amapes2@wisc.edu>
2025-01-28 08:36:34 -0600

    Updating of formatting and documentation

--- Data_Analysis.py
+++ Data_Analysis.py
@@ -1,165 +1,170 @@
+from data_generator import ensemble
 import matplotlib.pyplot as plt
-import pandas_datareader.data as pdr
 import numpy as np
-import pandas as pd
-import json # JSON is the JavaScript Object Notation library and is used in this code for converting python formats when working with the BLS API
-import requests # Requests allows for easy interactions with HTTP web servers/APIs
-from datetime import datetime
-import sys
-import inspect
 import os
-from io import StringIO
 
-
-
-def RMSE(Data1, Data2):
+def rmse(data1, data2):
     """
     The function calculates the normalized root mean squared error for 2 given data sets.
 
     Args:
-        Data1 (DataFrame): The first data set to be used
-        Data2 (DataFrame): The second data set to be used
+        data1 (dataFrame): The first data set to be used
+        data2 (dataFrame): The second data set to be used
 
     Returns:
         float: The value of the normalized root mean squared error
     """
-    Sum = 0
-    for i in range(0, len(Data1)):
-        Sum += (Data1[i] - Data2[i])**2/len(Data1)
+    sum = 0
+    for i in range(0, len(data1)):
+        sum += (data1[i] - data2[i])**2/len(data1)
         
-    return np.sqrt(Sum)/np.var(Data2)**(1/2)
+    return np.sqrt(sum)/np.var(data2)**(1/2)
 
 
 
-def Corr(Data1, Data2):
+def corr(data1, data2):
     """
-    Corr is a function that returns 2 data sets correlation
+    corr is a function that returns 2 data sets correlation
 
     Args:
-        Data1 (DataFrame): The first data set to be used
-        Data2 (DataFrame): The second data set to be used
+        data1 (dataFrame): The first data set to be used
+        data2 (dataFrame): The second data set to be used
 
     Returns:
         float: The value of the correlation between the two sets
     """
-    Sum_1, Sum_2, Sum_3 = 0, 0, 0
-    for i in range(0, len(Data1)):
-        Sum_1 += (Data1[i]-np.mean(Data1))*(Data2[i]-np.mean(Data2))
-        Sum_2 += (Data1[i]-np.mean(Data1))**2
-        Sum_3 += (Data2[i]-np.mean(Data2))**2
-    return Sum_1 / (np.sqrt(Sum_2) * np.sqrt(Sum_3))
+    sum_1, sum_2, sum_3 = 0, 0, 0
+    for i in range(0, len(data1)):
+        sum_1 += (data1[i]-np.mean(data1))*(data2[i]-np.mean(data2))
+        sum_2 += (data1[i]-np.mean(data1))**2
+        sum_3 += (data2[i]-np.mean(data2))**2
+    return sum_1 / (np.sqrt(sum_2) * np.sqrt(sum_3))
 
 
 
-def Poly_OLS_SDE(Data, Terms = 2):
+def poly_ols_sde(data, terms = 2):
     """
-    Poly_OLS_SDE is a function that uses a polynomial SDE form to model data.
+    poly_ols_SDE is a function that uses a polynomial SDE form to model data.
     The functional form of the model is defined as
         dX_t = (a*x**2+b*x+c) * dt
     where:
-        The highest order term is determined by the Terms parameter
+        The highest order term is determined by the terms parameter
 
     Args:
-        Data (DataFrame): The time series data to be used for this differential equation model
-        Terms (int, optional): The degree of the leading term in the polynomial. Defaults to 2.
+        data (dataFrame): The time series data to be used for this differential equation model
+        terms (int, optional): The degree of the leading term in the polynomial. Defaults to 2.
 
     Returns:
         list: Output is the coefficients for the model such as a,b,c from the example form above.
     """
 
-    dt = (Data[1,1]-Data[0,1])
-    n = len(Data[:,0])
-    x = Data[:,0] # Original Data
+    dt = (data[1,1]-data[0,1])
+    n = len(data[:,0])
+    x = data[:,0] # Original data
     dx = np.zeros((n, 1)) # Differntial series
-    mod = np.zeros((n, Terms)) # Model Data
+    mod = np.zeros((n, terms)) # Model data
             
     for i in range(0,n-1):
         dx[i+1] = (x[i+1] - x[i])/dt
-        for j in range(0, Terms):
+        for j in range(0, terms):
             mod[i+1,j] = x[i]**j
     
     return np.linalg.inv(mod.T @ mod) @ np.dot(mod.T, dx)
 
 
 
-def Poly_OLS(Data, Terms = 2):
-    n = len(Data[:,0])
-    X_mod = np.zeros((n, Terms))
+def poly_ols(data, terms = 2):
+    """
+    poly_ols_SDE is a function that uses a polynomial SDE form to model data.
+    The functional form of the model is defined as
+        dX_t = (a*x**2+b*x+c) * dt
+    where:
+        The highest order term is determined by the terms parameter
+
+    Args:
+        data (dataFrame): The time series data to be used for this differential equation model
+        terms (int, optional): The degree of the leading term in the polynomial. Defaults to 2.
+
+    Returns:
+        list: Output is the coefficients for the model such as a,b,c from the example form above.
+    """
+    n = len(data[:,0])
+    x_mod = np.zeros((n, terms))
     
     for i in range(0,n):
-        for j in range(0, Terms):
-            X_mod[i,j] = Data[i,0]**j
+        for j in range(0, terms):
+            x_mod[i,j] = data[i,0]**j
     
-    return np.linalg.inv(X_mod.T @ X_mod) @ np.dot(X_mod.T, Data[:,1])
+    return np.linalg.inv(x_mod.T @ x_mod) @ np.dot(x_mod.T, data[:,1])
 
 
 
-def Noise_Modeler(Data, Beta, Bins=1000, Terms=1):
-    dt = (Data[1,1]-Data[0,1])
-    n = len(Data[:,0])
-    X_noi = np.np.zeros((n, 1))
-    Noise = np.zeros((Bins, 2))
+def noise_modeler(data, beta, bins=1000, terms=1):
+    dt = (data[1,1]-data[0,1])
+    n = len(data[:,0])
+    x_noi = np.np.zeros((n, 1))
+    noise = np.zeros((bins, 2))
     
     for i in range(1, n):
         Est = 0
-        for j in range(0, len(Beta)):
-            Est += Beta[j] * Data[i-1,0]**j * dt
-        X_noi[i] = Data[i,0] - Data[i-1,0] - Est
+        for j in range(0, len(beta)):
+            Est += beta[j] * data[i-1,0]**j * dt
+        x_noi[i] = data[i,0] - data[i-1,0] - Est
     
-    Per = np.linspace(5, 95, (Bins+1))
-    Bin_edges = np.percentile(Data[:,0], Per)
-    Masks = {}
-    for i in range(0, Bins):
-        Mask = (Data[:,0] >= Bin_edges[i]) & (Data[:,0] <= Bin_edges[i+1])
-        Masks[f'bin_{i}'] = [Data[Mask,0], X_noi[Mask]]
+    per = np.linspace(5, 95, (bins+1))
+    bin_edges = np.percentile(data[:,0], per)
+    masks = {}
+    for i in range(0, bins):
+        mask = (data[:,0] >= bin_edges[i]) & (data[:,0] <= bin_edges[i+1])
+        masks[f'bin_{i}'] = [data[mask,0], x_noi[mask]]
     
-    for i in range(0, Bins):
-        Noise[i,0] = Masks[f'bin_{i}'][0].mean()
-        Noise[i,1] = (Masks[f'bin_{i}'][1].var()/dt)**(1/2)
-    Beta = Data_Analysis.Poly_OLS(Noise, Terms=Terms)
-    return Beta
+    for i in range(0, bins):
+        noise[i,0] = masks[f'bin_{i}'][0].mean()
+        noise[i,1] = (masks[f'bin_{i}'][1].var()/dt)**(1/2)
+    beta = poly_ols(noise, terms=terms)
+    return beta
 
 
 
-def Ensemble_Distribution(Ensemble):
-    n = len(Ensemble[:, 0])
+def ensemble_distribution(ensemble):
+    n = len(ensemble[:, 0])
     mean = np.zeros((n, 1))
     var = np.zeros((n, 1))
     for i in range(0, n):
-        mean[i] = np.mean(Ensemble[i,0:-1])
-        var[i] = np.var(Ensemble[i,0:-1])
+        mean[i] = np.mean(ensemble[i,0:-1])
+        var[i] = np.var(ensemble[i,0:-1])
     return mean, var
 
 
 
-def Ensemble_Forecast(Data, Lead, N=20, deterministic = 'y', noise = 'y', Bins = 1000, plot='on'):
-    if deterministic == 'y':
-        Beta = Data_Analysis.Poly_OLS_SDE(Data, Terms=2)
-        deterministic = f'{Beta[1]}*x+{Beta[0]}'
-        print(Beta)
+def ensemble_forecast(data, lead, N=20, drift = 'y', noise = 'y', bins = 1000, plot='on'):
+    if drift == 'y':
+        beta = poly_ols_sde(data, terms=2)
+        drift = f'{beta[1]}*x+{beta[0]}'
+        print(beta)
     if noise == 'y':
-        Sigma = Data_Analysis.Noise_Modeler(Data, Beta, Bins)
-        noise = f'{Sigma[1]}*x+{Sigma[0]}'
+        sigma = noise_modeler(data, beta, bins)
+        noise = f'{sigma[1]}*x+{sigma[0]}'
         
-    Time = int(np.round(Data[-1,1] - Data[0,1]))
-    n = Time+2-Lead
-    Points = np.zeros((n-1,2))
+    time = int(np.round(data[-1,1] - data[0,1]))
+    n = time+2-lead
+    points = np.zeros((n-1,2))
     for i in range(1, n):
-        Mask = np.round(Data[:,1], 5) == i-1
-        mean, var = Data_Analysis().Ensemble_Distribution(Data_Generator.Ensemble(Type='EM', N=N, deterministic = deterministic, noise = noise, T=Lead, X_0=float(Data[Mask, 0])))
-        Points[i-1, 1] = mean[-1]
+        mask = np.round(data[:,1], 5) == i-1
+        mean, var = ensemble_distribution(ensemble(method='em', N=N, drift = drift, noise = noise, t=lead, x_0=float(data[mask, 0])))
+        points[i-1, 1] = mean[-1]
         if plot == 'on':
-            plt.plot(np.array([i-1,i-1+Lead]), np.array([float(Data[Mask, 0]), Points[i-1,1]]), color='red', label="Ensemble Forecast" if i == 1 else "")
-            plt.scatter((i-1+Lead), Points[i-1, 1], color='black')
+            plt.plot(np.array([i-1,i-1+lead]), np.array([float(data[mask, 0]), points[i-1,1]]), color='red', label="ensemble forecast" if i == 1 else "")
+            plt.scatter((i-1+lead), points[i-1, 1], color='black')
     
     
-    for i in range(Lead, Time+1):
-        Mask = np.round(Data[:,1], 5) == i
-        Points[i-Lead, 0] = float(Data[Mask, 0])
+    for i in range(lead, time+1):
+        mask = np.round(data[:,1], 5) == i
+        points[i-lead, 0] = float(data[mask, 0])
         
     if plot == 'on': 
-        plt.plot(Data[:,1], Data[:,0], color = 'blue', label='Truth')
+        plt.plot(data[:,1], data[:,0], color = 'blue', label='Truth')
         plt.legend()
-        plt.title(f'{Lead} Second Lead Time Ensemble Forecasts')
+        plt.title(f'{lead} Second lead time ensemble forecasts')
         plt.show()
-    return Data_Analysis.Corr(Points[:,0], Points[:,1]), Data_Analysis.RMSE(Points[:,0], Points[:,1])
+    return corr(points[:,0], points[:,1]), rmse(points[:,0], points[:,1])
--- Data_Generator.py
+++ Data_Generator.py
@@ -1,23 +1,16 @@
-import matplotlib.pyplot as plt
-import pandas_datareader.data as pdr
+
 import numpy as np
-import pandas as pd
-import json # JSON is the JavaScript Object Notation library and is used in this code for converting python formats when working with the BLS API
-import requests # Requests allows for easy interactions with HTTP web servers/APIs
-from datetime import datetime
-import sys
 import inspect
 import os
-from io import StringIO
 
 
 
-def Forward_Euler(drift = 'y', dt = 0.01, T = 50, X_0 = 0):
+def forward_euler(drift = 'y', dt = 0.01, t = 50, x_0 = 0):
     """
-    Forward Euler function is used to simulate data points of a deterministic differential equation.
+    forward euler function is used to simulate data points of a deterministic differential equation.
     It works by requesting the string of your function. If the parameter entry is not used.
     Then using the eval function and a lambda expression string, the function is created and run
-    through the forward Euler equations to generate the desired data
+    through the forward euler equations to generate the desired data
 
     The SDE is defined as:
         dX_t = drift(X_t, t) * dt
@@ -30,9 +23,9 @@ def Forward_Euler(drift = 'y', dt = 0.01, T = 50, X_0 = 0):
             that is to be simulated
         dt. TYPE: float
             The entry here is for the time step
-        T. TYPE: int
+        t. TYPE: int
             The time duration of the simulated data
-        X_0. TYPE: float
+        x_0. TYPE: float
             The initial value of the simulated data    
     """
 
@@ -40,20 +33,20 @@ def Forward_Euler(drift = 'y', dt = 0.01, T = 50, X_0 = 0):
         drift = input("Enter your differential equation (e.g., 'x**2 + 2'): ")
     drift = eval(f"lambda x: {drift}")
     
-    steps = int(np.round(T/dt)) + 1
-    X = np.zeros((steps, 3))
-    X[0, 0] = X_0
+    steps = int(np.round(t/dt)) + 1
+    x = np.zeros((steps, 3))
+    x[0, 0] = x_0
     
     for i in range(0,steps-1):
-        X[i+1,0] = X[i,0] + drift(X[i,0]) * dt
-        X[i+1,1] = X[i,1] + dt
-    return X
+        x[i+1,0] = x[i,0] + drift(x[i,0]) * dt
+        x[i+1,1] = x[i,1] + dt
+    return x
 
 
 
-def Euler_Maruyama(drift = 'y', diffusion = 'y', dt = 0.01, T = 50, X_0 = 0):
+def euler_maruyama(drift = 'y', diffusion = 'y', dt = 0.01, t = 50, x_0 = 0):
     """
-    Simulates sample paths of a stochastic differential equation (SDE) using the Euler-Maruyama method.
+    Simulates sample paths of a stochastic differential equation (SDE) using the euler-maruyama method.
 
     The SDE is defined as:
         dX_t = drift(X_t, t) * dt + diffusion(X_t, t) * dW_t
@@ -73,9 +66,9 @@ def Euler_Maruyama(drift = 'y', diffusion = 'y', dt = 0.01, T = 50, X_0 = 0):
             from the input prompts after the function is run.
         dt. TYPE: float
             The entry here is for the time step
-        T. TYPE: int
+        t. TYPE: int
             The time duration of the simulated data
-        X_0. TYPE: float
+        x_0. TYPE: float
             The initial value of the simulated data
     """
 
@@ -93,28 +86,28 @@ def Euler_Maruyama(drift = 'y', diffusion = 'y', dt = 0.01, T = 50, X_0 = 0):
     else:
         diffusion = eval(f"lambda x: {diffusion}")
     
-    steps = int(np.round(T/dt)) + 1
-    X = np.zeros((steps, 3))
-    X[0, 0] = X_0
+    steps = int(np.round(t/dt)) + 1
+    x = np.zeros((steps, 3))
+    x[0, 0] = x_0
     
     for i in range(0,steps-1):
-        X[i+1,1] = X[i,1] + dt
-        X[i+1,2] = np.sqrt(dt) * np.random.normal(0,1)
+        x[i+1,1] = x[i,1] + dt
+        x[i+1,2] = np.sqrt(dt) * np.random.normal(0,1)
         if "t" in inspect.signature(drift).parameters:
-            det = drift(X[i,0], X[i,1])
+            det = drift(x[i,0], x[i,1])
         else:
-            det = drift(X[i,0])
+            det = drift(x[i,0])
         if "t" in inspect.signature(diffusion).parameters:
-            sto = diffusion(X[i,0], X[i,1])
+            sto = diffusion(x[i,0], x[i,1])
         else:
-            sto = diffusion(X[i,0])
-        X[i+1,0] = X[i,0] + det * dt + sto * X[i,2]
+            sto = diffusion(x[i,0])
+        x[i+1,0] = x[i,0] + det * dt + sto * x[i,2]
         
-    return X
+    return x
 
 
 
-def Milstein(drift = 'y', diffusion = 'y', diff_diffusion = 'y', dt = 0.01, T = 50, X_0 = 0):
+def milstein(drift = 'y', diffusion = 'y', diff_diffusion = 'y', dt = 0.01, t = 50, x_0 = 0):
     """
     Simulates sample paths of a stochastic differential equation (SDE) using the Milstein method.
 
@@ -140,9 +133,9 @@ def Milstein(drift = 'y', diffusion = 'y', diff_diffusion = 'y', dt = 0.01, T = 
             is run. 
         dt. TYPE: float
             The entry here is for the time step
-        T. TYPE: int
+        t. TYPE: int
             The time duration of the simulated data
-        X_0. TYPE: float
+        x_0. TYPE: float
             The initial value of the simulated data
     """
     if drift == 'y':
@@ -155,22 +148,22 @@ def Milstein(drift = 'y', diffusion = 'y', diff_diffusion = 'y', dt = 0.01, T = 
         diff_diffusion = input("Enter the stochastic portion of your SDE's derivative (e.g., 'x**2 + 2'): ")
     diff_diffusion = eval(f"lambda x: {diff_diffusion}")
     
-    steps = int(np.round(T/dt)) + 1
-    X = np.zeros((steps, 3))
-    X[0, 0] = X_0
+    steps = int(np.round(t/dt)) + 1
+    x = np.zeros((steps, 3))
+    x[0, 0] = x_0
     
     for i in range(0,steps-1):
-        X[i+1,2] = np.sqrt(dt) * np.random.normal(0,1)
-        X[i+1,0] = X[i,0] + drift(X[i,0]) * dt + diffusion(X[i,0]) * X[i,2] + (1/2) * diffusion(X[i,0]) * diff_diffusion(X[i,0]) * (X[i,2]**2-dt**2)
-        X[i+1,1] = X[i,1] + dt
-    return X
+        x[i+1,2] = np.sqrt(dt) * np.random.normal(0,1)
+        x[i+1,0] = x[i,0] + drift(x[i,0]) * dt + diffusion(x[i,0]) * x[i,2] + (1/2) * diffusion(x[i,0]) * diff_diffusion(x[i,0]) * (x[i,2]**2-dt**2)
+        x[i+1,1] = x[i,1] + dt
+    return x
 
 
 
-def Ensemble(method = 'EM', N = 10, drift = 'y', diffusion = 'y', dt = 0.01, T = 50, X_0 = 0):
+def ensemble(method = 'EM', N = 10, drift = 'y', diffusion = 'y', dt = 0.01, t = 50, x_0 = 0):
     """
-    Ensemble function is used to create a set of ensemble members for analysis from an (SDE) when a singular
-    sample path is not sufficient. It is capable of either using Euler-Maruyama method or Forward Euler 
+    ensemble function is used to create a set of ensemble members for analysis from an (SDE) when a singular
+    sample path is not sufficient. It is capable of either using euler-maruyama method or forward euler 
     depending on if there exists a stochastic portion of the SDE.
 
     The SDE is defined as:
@@ -182,8 +175,8 @@ def Ensemble(method = 'EM', N = 10, drift = 'y', diffusion = 'y', dt = 0.01, T =
 
     PARAMETERS:
         method. TYPE: str
-            method may either be 'EM' for Euler-Maruyama method of data generation for an SDE, or 'FE' for
-            Forward Euler method of data generation for an ODE.
+            method may either be 'EM' for euler-maruyama method of data generation for an SDE, or 'FE' for
+            forward euler method of data generation for an ODE.
         N. TYPE: int
             N is the number of ensemble members desired. 
         drift. TYPE: str
@@ -196,37 +189,37 @@ def Ensemble(method = 'EM', N = 10, drift = 'y', diffusion = 'y', dt = 0.01, T =
             from the input prompts after the function is run.
         dt. TYPE: float
             The entry here is for the time step
-        T. TYPE: int
+        t. TYPE: int
             The time duration of the simulated data
-        X_0. TYPE: float
+        x_0. TYPE: float
             The initial value of the simulated data
     """
-    n = int(T/dt) + 1
+    n = int(t/dt) + 1
     if method == 'FE':
         if drift == 'y':
             drift = input("Enter the drift portion of your SDE (e.g., 'x**2 + 2'): ")
-        Data = np.tile([0.0]*(N+1), (n,1))
+        data = np.tile([0.0]*(N+1), (n,1))
         for i in range(0,N):
-            X = Data_Generator.Forward_Euler(drift = drift, dt = dt, T = T, X_0 = X_0)
-            Data[:,i] = X[:,0]
-        Data[:,N] = X[:,1]
+            x = forward_euler(drift = drift, dt = dt, t = t, x_0 = x_0)
+            data[:,i] = x[:,0]
+        data[:,N] = x[:,1]
     
     elif method == 'EM':
         if drift == 'y':
             drift = input("Enter the drift portion of your SDE (e.g., 'x**2 + 2'): ")
         if diffusion == 'y':
             diffusion = input("Enter the stochastic portion of your SDE (e.g., 'x**2 + 2'): ")
-        Data = np.tile([0.0]*(N+1), (n,1))
+        data = np.tile([0.0]*(N+1), (n,1))
         for i in range(0,N):
-            X = Data_Generator.Euler_Maruyama(drift = drift, diffusion = diffusion, dt = dt, T = T, X_0 = X_0)
-            Data[:,i] = X[:,0]
-        Data[:,N] = X[:,1]
+            x = euler_maruyama(drift = drift, diffusion = diffusion, dt = dt, t = t, x_0 = x_0)
+            data[:,i] = x[:,0]
+        data[:,N] = x[:,1]
         
-    return Data
+    return data
 
 
 
-def Ordinary_Equation(equation = 'y', diffusion = 'y', dt = 0.01, T = 50, X_0 = 0):
+def Ordinary_Equation(equation = 'y', diffusion = 'y', dt = 0.01, t = 50, x_0 = 0):
     if equation == 'y':
         equation = input("Enter the Ordinary Equation (e.g., 'x**2 + 2'): ")
     else:
@@ -237,16 +230,16 @@ def Ordinary_Equation(equation = 'y', diffusion = 'y', dt = 0.01, T = 50, X_0 = 
     else:
         diffusion = eval(f"lambda x: {diffusion}")
     
-    steps = int(np.round(T/dt)) + 1
-    X = np.zeros((steps, 3))
-    X[0, 0] = X_0
-    Time = 0
+    steps = int(np.round(t/dt)) + 1
+    x = np.zeros((steps, 3))
+    x[0, 0] = x_0
+    time = 0
     for i in range(0,steps-1):
-        Time += dt
-        X[i,1] = Time
-        X[i,2] = np.sqrt(dt) * np.random.normal(0,1)
-        X[i,0] = equation(Time) + diffusion(equation(Time)) * X[i,2]
-    return X
+        time += dt
+        x[i,1] = time
+        x[i,2] = np.sqrt(dt) * np.random.normal(0,1)
+        x[i,0] = equation(time) + diffusion(equation(time)) * x[i,2]
+    return x
 
 
 
